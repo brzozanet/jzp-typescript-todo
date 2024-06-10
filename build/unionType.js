84564c58abc50ff4dd9b8ee53b22ddcfeba9bc01
Paweł Brzozowski <pawel@brzoza.net>
2024-06-10 19:16:30 +0200

    unionType

---
+++ build/unionType.js
@@ -0,0 +1,5 @@
+let logAage = (age) => {
+    console.log(`Hej! Mam ${age} lat!`);
+};
+logAage(47);
+logAage("czterdzieści siedem");
---
+++ src/unionType.ts
@@ -0,0 +1,6 @@
+let logAage = (age: number | string) => {
+  console.log(`Hej! Mam ${age} lat!`);
+};
+
+logAage(47);
+logAage("czterdzieści siedem");
