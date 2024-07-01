fde658ae4199c2381e9b68aeca30ae5d81ca6644
Paweł Brzozowski <pawel@brzoza.net>
2024-07-01 21:45:07 +0200

    renderTasks

--- build/app.js
+++ build/app.js
@@ -1,1 +1,20 @@
-console.log("Hello Typescript");
+// <li>
+//   <label for="task-1">Nauczyć się Typescript</label>
+//   <input type="checkbox" id="task-1" name="Nauczyć się Typescript" />
+// </li>
+const tasksContainerElement = document.querySelector(".tasks");
+const tasks = [
+    "Nauczyć się Typescript",
+    "Nauczyć sie Next.js",
+    "Skończyć zaległy kurs React",
+    "Zrobić portfolio",
+    "Znaleźć pracę",
+];
+const renderTasks = () => {
+    tasks.forEach((task) => {
+        const taskElement = document.createElement("li");
+        taskElement.innerText = task;
+        tasksContainerElement.append(taskElement);
+    });
+};
+renderTasks();
--- src/app.ts
+++ src/app.ts
@@ -1,1 +1,25 @@
-console.log("Hello Typescript");
+// <li>
+//   <label for="task-1">Nauczyć się Typescript</label>
+//   <input type="checkbox" id="task-1" name="Nauczyć się Typescript" />
+// </li>
+
+const tasksContainerElement: HTMLUListElement =
+  document.querySelector(".tasks");
+
+const tasks: Array<string> = [
+  "Nauczyć się Typescript",
+  "Nauczyć sie Next.js",
+  "Skończyć zaległy kurs React",
+  "Zrobić portfolio",
+  "Znaleźć pracę",
+];
+
+const renderTasks = () => {
+  tasks.forEach((task) => {
+    const taskElement: HTMLLIElement = document.createElement("li");
+    taskElement.innerText = task;
+    tasksContainerElement.append(taskElement);
+  });
+};
+
+renderTasks();
