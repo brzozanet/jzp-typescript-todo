33895086f521e76682c8eeb0238f8510dc61610f
Paweł Brzozowski <pawel@brzoza.net>
2024-07-02 09:25:52 +0200

    Minor fix

--- src/app.ts
+++ src/app.ts
@@ -1,8 +1,3 @@
-// <li>
-//   <label for="task-1">Nauczyć się Typescript</label>
-//   <input type="checkbox" id="task-1" name="Nauczyć się Typescript" />
-// </li>
-
 const tasksContainerElement: HTMLUListElement =
   document.querySelector(".tasks");
 const taskNameInputElement: HTMLInputElement = document.querySelector("#name");
@@ -46,14 +41,56 @@ const tasks: {
   },
 ];
 
+// <li>
+//   <label for="task-1">Nauczyć się Typescript</label>
+//   <input type="checkbox" id="task-1" name="Nauczyć się Typescript" />
+// </li>
+
+// NOTE: Opcja 1 jest bardziej tradycyjna i może być preferowana ze względów bezpieczeństwa,
+// ponieważ tworzy elementy DOM programowo, co minimalizuje ryzyko ataków typu Cross-Site Scripting (XSS),
+// gdy dane pochodzą z niezaufanych źródeł. Pozwala na bardziej kontrolowane tworzenie elementów i atrybutów,
+// ale jest bardziej rozwlekła i wymaga więcej kodu do osiągnięcia tego samego efektu.
+
 const renderTasks = () => {
   tasksContainerElement.innerHTML = "";
-  tasks.forEach((task) => {
+  tasks.forEach((task, index) => {
     const taskElement: HTMLLIElement = document.createElement("li");
+    const labelElement: HTMLLabelElement = document.createElement("label");
+    const taskId: string = `task-${index}`;
+    const checkboxElement: HTMLInputElement = document.createElement("input");
+
     taskElement.innerText = task.name;
+    labelElement.setAttribute("for", taskId);
+    labelElement.setAttribute("name", task.name);
+
+    checkboxElement.type = "checkbox";
+    checkboxElement.name = task.name;
+    checkboxElement.id = taskId;
+
+    taskElement.append(labelElement, checkboxElement);
     tasksContainerElement.append(taskElement);
   });
 };
+
+// NOTE: Opcja 2 jest nowsza i bardziej zwięzła, ponieważ pozwala na bezpośrednie wstrzyknięcie HTML
+// do elementu DOM za pomocą jednej instrukcji. Używa literałów szablonowych (template literals), co sprawia,
+// że jest bardziej czytelna i pozwala na łatwe włączenie zmiennych do znaczników HTML.
+// Jest to szczególnie przydatne przy budowaniu skomplikowanych struktur HTML.
+
+// const renderTasks = () => {
+//   tasksContainerElement.innerHTML = "";
+//   tasks.forEach((task, index) => {
+//     const taskElement: HTMLLIElement = document.createElement("li");
+//     const taskId: string = `task-${index}`;
+
+//     taskElement.innerHTML = `
+//       <label for="task-${taskId}">${task.name}</label>
+//       <input type="checkbox" id="task-${taskId}" name="${task.name}" />
+//     `;
+
+//     tasksContainerElement.append(taskElement);
+//   });
+// };
 
 const addTask = (taskName: string) => {
   tasks.push({ name: taskName, done: false });
