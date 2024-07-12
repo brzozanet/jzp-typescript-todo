fcb9b3ca55cbfc1c0541910625400ce4c47a3dca
Paweł Brzozowski <pawel@brzoza.net>
2024-07-12 16:06:33 +0200

    Type: array

--- build/app.js
+++ build/app.js
@@ -1,3 +1,9 @@
+const taskContainerElement = document.querySelector(".tasks");
+console.log(taskContainerElement);
+const taskNameInputElement = document.querySelector("#name");
+console.log(taskNameInputElement.value);
+const addTaskButtonElement = document.querySelector("button");
+console.log(addTaskButtonElement);
 const tasks = [
     "Nauczyć się Typescript",
     "Nauczyć sie Next.js",
@@ -5,3 +11,20 @@ const tasks = [
     "Zrobić portfolio",
     "Znaleźć pracę",
 ];
+const tasksRender = () => {
+    taskContainerElement.innerText = "";
+    tasks.forEach((task) => {
+        const taskElement = document.createElement("li");
+        taskElement.innerText = task;
+        taskContainerElement.appendChild(taskElement);
+    });
+};
+const addTask = (task) => {
+    tasks.push(task);
+};
+addTaskButtonElement.addEventListener("click", (event) => {
+    event.preventDefault();
+    addTask(taskNameInputElement.value);
+    tasksRender();
+});
+tasksRender();
--- src/app.ts
+++ src/app.ts
@@ -1,3 +1,13 @@
+const taskContainerElement: HTMLElement = document.querySelector(".tasks");
+console.log(taskContainerElement);
+
+const taskNameInputElement: HTMLInputElement = document.querySelector("#name");
+console.log(taskNameInputElement.value);
+
+const addTaskButtonElement: HTMLButtonElement =
+  document.querySelector("button");
+console.log(addTaskButtonElement);
+
 const tasks: string[] = [
   "Nauczyć się Typescript",
   "Nauczyć sie Next.js",
@@ -5,3 +15,24 @@ const tasks: string[] = [
   "Zrobić portfolio",
   "Znaleźć pracę",
 ];
+
+const tasksRender = () => {
+  taskContainerElement.innerText = "";
+  tasks.forEach((task) => {
+    const taskElement: HTMLElement = document.createElement("li");
+    taskElement.innerText = task;
+    taskContainerElement.appendChild(taskElement);
+  });
+};
+
+const addTask = (task: string) => {
+  tasks.push(task);
+};
+
+addTaskButtonElement.addEventListener("click", (event: MouseEvent) => {
+  event.preventDefault();
+  addTask(taskNameInputElement.value);
+  tasksRender();
+});
+
+tasksRender();
