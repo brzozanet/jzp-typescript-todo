c61283ad081ced146094686043713287a8bf3d4b
Paweł Brzozowski <pawel@brzoza.net>
2024-08-16 15:36:50 +0200

    Enums

--- build/types/types.js
+++ build/types/types.js
@@ -1,1 +1,8 @@
 export {};
+// export enum Category {
+//   OGOLNE = "ogolne",
+//   KURSY = "kursy",
+//   MENTORING = "mentoring",
+//   SOFTSKILL = "softskill",
+// }
+// const category = Category.KURSY;
--- src/types/types.ts
+++ src/types/types.ts
@@ -1,1 +1,10 @@
 export type Categories = "ogolne" | "kursy" | "mentoring" | "softskill";
+
+// export enum Category {
+//   OGOLNE = "ogolne",
+//   KURSY = "kursy",
+//   MENTORING = "mentoring",
+//   SOFTSKILL = "softskill",
+// }
+
+// const category = Category.KURSY;
